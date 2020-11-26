5a5d6059f1f6b11f17a29e914078c2042900aa3b
Yuliya Babitskaya <bambina-kissi@yandex.ru>
2020-11-26 21:48:33 +0300

    add function

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 'use strict';
 
-function guessNumber() {
-    let numGame = 5;
-    let numUser = +prompt('Угадай число от 1 до 100');
-    if(numUser > 0 && numUser < numGame) {
-        alert('Загаданное число меньше');
-        guessNumber();
-    } else if (numUser > numGame){
-        alert('Загаданное число больше');
-        guessNumber();
-    } else if (isNaN(numUser)) {
-        alert('Введи число!');
-        guessNumber();
-    } else if (numUser === 0) {
-        alert('Игра окончена');
-    } else if (numUser === numGame) {
-        alert('Поздравляю, Вы угадали!!!');
+function rondomNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    let numGame = Math.floor(Math.random() * (max - min + 1)) + min;
+    console.log(numGame);
+    function guessNumber(numGame) {
+        let numUser = +prompt('Угадай число от 1 до 100');
+        if(numUser > 0 && numUser < numGame) {
+            alert('Загаданное число меньше');
+            guessNumber(numGame);
+        } else if (numUser > numGame){
+            alert('Загаданное число больше');
+            guessNumber(numGame);
+        } else if (isNaN(numUser)) {
+            alert('Введи число!');
+            guessNumber(numGame);
+        } else if (numUser === 0) {
+            alert('Игра окончена');
+        } else if (numUser === numGame) {
+            alert('Поздравляю, Вы угадали!!!');
+        }
     }
+    guessNumber(numGame);
 }
 
-guessNumber();
+rondomNumber(1, 100);
