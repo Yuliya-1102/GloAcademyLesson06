f3bdf5bac9de19aee219fd508c7c87e1f11b1f9b
Yuliya Babitskaya <bambina-kissi@yandex.ru>
2020-12-02 13:21:54 +0300

    add task lesson10

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
 'use strict';
 
-function rondomNumber(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    let numGame = Math.floor(Math.random() * (max - min + 1)) + min;
-    console.log(numGame);
-    function guessNumber(numGame) {
-        let numUser = +prompt('Угадай число от 1 до 100');
-        if(numUser > 0 && numUser < numGame) {
-            alert('Загаданное число меньше');
-            guessNumber(numGame);
-        } else if (numUser > numGame){
-            alert('Загаданное число больше');
-            guessNumber(numGame);
-        } else if (isNaN(numUser)) {
-            alert('Введи число!');
-            guessNumber(numGame);
-        } else if (numUser === 0) {
-            alert('Игра окончена');
-        } else if (numUser === numGame) {
-            alert('Поздравляю, Вы угадали!!!');
-        }
-    }
-    guessNumber(numGame);
-}
+const collection = document.querySelectorAll('ul');
+const elems = document.querySelectorAll('li');
+const adv = document.querySelector('.adv');
+const book = document.querySelectorAll('.book');
+const body = document.querySelector('body');
+const h2 = document.querySelectorAll('h2 a');
+console.log('h2: ', h2);
 
-rondomNumber(1, 100);
+console.log('body: ', body);
+
+console.log('book: ', book);
+
+
+book[0].before(book[1]);
+book[2].before(book[4]);
+book[5].after(book[2]);
+
+body.style.backgroundImage = 'url(./image/you-dont-know-js.jpg)';
+
+h2[4].textContent = 'Книга 3. this и Прототипы Объектов';
+
+adv.remove();
+
+elems[3].after(elems[6]);
+elems[4].before(elems[8]);
+elems[9].after(elems[2]);
+
+elems[48].before(elems[55]);
+elems[50].after(elems[48]);
+elems[53].after(elems[51]);
+
+const elemTitle = elems[25].cloneNode(true);
+elemTitle.textContent = 'Глава 8: За пределами ES6';
+elems[25].after(elemTitle);
+// console.log(collection[0]);
+console.log(elems);
+
